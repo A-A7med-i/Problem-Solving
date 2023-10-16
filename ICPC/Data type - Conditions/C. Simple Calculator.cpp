7a0a12b0606473f08aa684a3c96952a793cea248
A7med nabil <an223602@gmail.com>
2023-10-16 10:15:05 +0300

    First Sheet

---
+++ ICPC/Data type - Conditions/C. Simple Calculator.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int long long X,Y;
+    cin>>X>>Y;
+    int sum = X + Y;
+    int long long multiplication = X * Y;
+    int subtraction = X - Y;
+    cout<<X<<" + "<<Y<<" = "<<sum<<endl;
+    cout<<X<<" * "<<Y<<" = "<<multiplication<<"\n";
+    cout<<X<<" - "<<Y<<" = "<<subtraction<<endl;
+}
+
+
+
