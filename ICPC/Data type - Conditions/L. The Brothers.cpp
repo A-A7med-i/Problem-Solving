1382f3a1d0e1acbe4a3c611f79ddbe381a400334
A7med nabil <an223602@gmail.com>
2023-10-20 22:51:03 +0300

    First Sheet

---
+++ ICPC/Data type - Conditions/L. The Brothers.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+  string first_name_1,second_name_1;
+  string first_name_2,second_name_2;
+  cin>>first_name_1>>second_name_1>>first_name_2>>second_name_2;
+  cout<<(second_name_1==second_name_2?"ARE Brothers":"NOT");
+}
