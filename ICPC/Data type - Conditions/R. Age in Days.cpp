67768a9d4756b7e2f6c728f1363a5cea1aee81a5
A7med nabil <an223602@gmail.com>
2023-10-24 22:17:36 +0300

    First Sheet

---
+++ ICPC/Data type - Conditions/R. Age in Days.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+  long long n;
+  cin>>n;
+  cout<<n/365<<" years"<<endl;
+  cout<<n%365/30<<" months"<<endl;
+  cout<<(n%365)%30<<" days"<<endl;
+}
